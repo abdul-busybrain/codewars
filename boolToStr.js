81551b02b738e9bfaa1a5a36bd5747cfbd2edaf1
abdul-busybrain <awwalhajine@gmail.com>
2024-12-31 05:43:36 +0100

    Boolean to String Kata

---
+++ boolToStr.js
@@ -0,0 +1,12 @@
+/**
+NOTE KATA
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+
+NOTE TOPICS
+Strings Fundamentals
+ */
+function booleanToString(b) {
+  return b.toString();
+}
