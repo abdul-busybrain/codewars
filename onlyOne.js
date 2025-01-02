4850e783cb549d2663a95c015634924407c3961b
abdul-busybrain <awwalhajine@gmail.com>
2025-01-02 06:59:38 +0100

    Only Need One Solved

---
+++ onlyOne.js
@@ -0,0 +1,11 @@
+/**
+NOTE KATA
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+a can contain numbers or strings. x can be either.
+
+Return true if the array contains the value, false if not.
+
+NOTE TOPICS
+Fundamentals Strings Arrays 
+ */
